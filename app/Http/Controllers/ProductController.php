4b9c2fb29528596d3f09a5a097519356bae823b8
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2024-11-29 14:49:30 -0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,7 +43,6 @@ class ProductController extends Controller
 
     public function updateProd(Request $request, Product $product){
         
-        
         $data = $request->validate([
             'name' => 'required',
             'qty' => 'required|numeric',
