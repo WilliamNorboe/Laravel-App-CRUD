d874bfd2bf0dbf4b6079d903400bb13a23cccc14
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2024-11-30 12:50:50 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ Route::get('/product', [ProductController::class, 'index'])->name('product.index
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 
-
 Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
 Route::put('/product/{product}', [ProductController::class, 'updateProd'])->name('product.updateProd');
 Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
