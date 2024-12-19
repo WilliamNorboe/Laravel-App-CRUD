0cbff354c3c3baac0d7e26aee42915cb12022167
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2024-12-19 13:52:44 -0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    protected $hidden = [ // make sure password is hidden
         'password',
         'remember_token',
     ];
