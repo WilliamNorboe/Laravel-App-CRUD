02e656745e752cbf6881b22be935999c7fd3ef18
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2025-02-24 18:10:07 -0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    // 
+    //
     public function index(){
         $products = Product::all();
         return view('products.index', ['products'=>$products]);
